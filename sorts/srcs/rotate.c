07b5cb0c92b4c229e02dbb613cb34480ef44922c
Luzzie <estelarodrigoluz@gmail.com>
2026-05-28 10:23:33 +0100

    save

--- sorts/srcs/rotate.c
+++ sorts/srcs/rotate.c
@@ -6,7 +6,7 @@
 /*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 12:50:05 by rodrpere          #+#    #+#             */
-/*   Updated: 2026/05/27 16:55:17 by rodrpere         ###   ########.fr       */
+/*   Updated: 2026/05/28 10:23:26 by rodrpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,34 @@
 
 void	ra(t_list **stack_a)
 {
-	t_list *tail;
+	t_list	*tail;
 
 	if (!*stack_a || !(*stack_a)->next)
 		return ;
 	tail = ft_lstlast(*stack_a);
+	tail->prev->next = NULL;
+	(*stack_a)->prev = tail;
 	tail->next = *stack_a;
+	tail->prev = NULL;
+	(*stack_a) = tail;
 }
 
-void	rb(t_list *stack_b)
+void	rb(t_list **stack_b)
 {
-	t_list *tail;
-	
-	tail = ft_lstlast(stack_b);
-	tail->next = stack_b;
-	stack_b->next = NULL;
+	t_list	*tail;
+
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	tail = ft_lstlast(*stack_b);
+	tail->prev->next = NULL;
+	(*stack_b)->prev = tail;
+	tail->next = *stack_b;
+	tail->prev = NULL;
+	(*stack_b) = tail;
 }
 
 void	rr(t_list *stack_a, t_list *stack_b)
 {
 	ra(&stack_a);
-	rb(stack_b);
+	rb(&stack_b);
 }
